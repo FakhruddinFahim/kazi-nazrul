25795371562b728595bbe3be63d1bf45bbf74c56
Fakhruddin Fahim <fakhruddinfahim@gmail.com>
2024-12-12 21:55:15 +0600

    remove dist from .gitignore

---
+++ dist/config.php
@@ -0,0 +1,6 @@
+<?php
+
+$smtp_host = "fakhr.local"; //SMTP Host
+$username = "test@fakhr.local"; //SMTP Username
+$password = "12345678"; //SMTP Password
+$to_address = "me@fakhr.local"; //address to receive email
